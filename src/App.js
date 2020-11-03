2e36a0306c95ceed7b1807316d7ed705089a77e0
daye <leeda1801@gmail.com>
2020-11-03 20:14:06 +0900

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Home</button>
+      <button>About me</button>
+      <button>Ability</button>
+      <button>Portfolio</button>
+      <button>Contact</button>
     </div>
   );
 }
