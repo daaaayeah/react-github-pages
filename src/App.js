539f58f7452a5af52ccefc5e84924e565d0ca38a
daye <leeda1801@gmail.com>
2020-11-04 15:38:15 +0900

    Modify logo letter-spacing

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const logoStyle = {
-    color: "white",
-    backgroundColor: "transparent",
-    fontSize: "25px",
-    fontFamily: "Arial",
-    fontWeight: "bold",
-    padding: "30px 200px 30px",
-    borderStyle: "none"
-  };
-
   return (
     <div className="App">
 
       <div style={{backgroundColor: "green", display: "flex", justifyContent: "space-between"}}>
         <div>
-          <button style={logoStyle}>
+          <button className="logo">
             PORTFOLIO
           </button>
         </div>
