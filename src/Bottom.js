d84c92525913b01da1e8e91f0a90bb734adad1e0
daaaayeah <52729559+daaaayeah@users.noreply.github.com>
2020-11-05 01:44:53 +0900

    Add files via upload

---
+++ src/Bottom.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Bottom extends Component {
+    render() {
+        return (
+            <Contact>
+
+            </Contact>
+        )
+    }
+}
+
+const Contact = styled.div`
+
+`;
+
+export default Bottom;
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Main extends Component {
+    render() {
+      return (
+        <Div>
+
+        </Div>
+      )
+    }
+}
+
+const Div = styled.div`
+
+`;
+
+export default Main;
