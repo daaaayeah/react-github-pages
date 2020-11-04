ff50fb8e75b3c890d39f898a7dfffc29d255ec09
daye <leeda1801@gmail.com>
2020-11-05 01:14:26 +0900

    Modify navigation bar CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 12df821cf51593d57ed333b7fddca64940b51aa5
 import NavigationBar from './NavigationBar';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div>
+=======
+    <div className="App">
+>>>>>>> 12df821cf51593d57ed333b7fddca64940b51aa5
       <NavigationBar />
     </div>
   );
