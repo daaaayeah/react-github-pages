82fa81856b6394372a78538c4ae34f569f2a8012
daye <leeda1801@gmail.com>
2020-11-05 01:01:35 +0900

    Delete unnecessary lines

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import NavigationBar from './NavigationBar';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavigationBar />
     </div>
   );
