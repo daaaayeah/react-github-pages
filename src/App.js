12df821cf51593d57ed333b7fddca64940b51aa5
daye <leeda1801@gmail.com>
2020-11-04 20:13:09 +0900

    Modify navigation bar CSS using styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
-
-      <div style={{backgroundColor: "green", display: "flex", justifyContent: "space-between"}}>
-        <div>
-          <button className="logo">
-            PORTFOLIO
-          </button>
-        </div>
-
-        <div style={{paddingRight: "200px"}}>
-          <button className="button">Home</button>
-          <button className="button">About</button>
-          <button className="button">Ability</button>
-          <button className="button">Portfolio</button>
-          <button className="button">Contact</button>
-        </div>
-      </div>
-      
+      <NavigationBar />
     </div>
   );
 }
---
+++ src/NavigationBar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class NavigationBar extends Component {
+  render() {
+    return (
+        <Navi>
+            <Logo logo>PORTFOLIO</Logo>
+            <Menu>
+              <Button home>Home</Button>
+              <Button about>About</Button>
+              <Button ability>Ability</Button>
+              <Button portfolio>Portfolio</Button>
+              <Button contact>Contact</Button>
+            </Menu>
+        </Navi>
+    )
+  }
+}
+
+const Navi = styled.div`
+  background-color: green;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Logo = styled.button`
+  color: white;
+  background-color: transparent;
+  font-size: 25px;
+  font-family: Roboto;
+  font-weight: bold;
+  letter-spacing: 3px;
+  padding: 30px 200px 30px;
+  border-style: none;
+  cursor: pointer;
+  outline: none;
+`;
+
+const Menu = styled.div`
+  padding-right: 200px;
+`;
+
+const Button = styled.button`
+  color: #c8dbc8;
+  background-color: transparent;
+  font-size: 15px;
+  font-family: Arial;
+  padding: 37px 20px 37px 20px;
+  border-style: none;
+  cursor: pointer;
+  outline: none;
+
+  &:hover {
+    color: white;
+  }
+`;
+
+export default NavigationBar;
