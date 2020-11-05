504787cabbbe6ec12095832499c3ccec04cb3907
daye <leeda1801@gmail.com>
2020-11-05 14:24:08 +0900

    Add contact area

--- src/Bottom.js
+++ src/Bottom.js
@@ -5,14 +5,26 @@ class Bottom extends Component {
     render() {
         return (
             <Contact>
-
+                <div>
+                    <P>CONTACT</P>
+                </div>
             </Contact>
         )
     }
 }
 
 const Contact = styled.div`
+  background-color: black;
+  height: 300px;
+  width: 100%;
+  display: inline-block;
+`;
 
+const P = styled.p`
+  color: white;
+  background-color: transparent;
+  font-size: 25px;
+  font-family: Roboto;
 `;
 
 export default Bottom;
