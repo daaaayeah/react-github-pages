4e0ce3c4b9f90296213e3877ea81ff69fefa5ed6
daye <leeda1801@gmail.com>
2020-11-12 16:24:10 +0900

    Add about title

--- src/Body.js
+++ src/Body.js
@@ -5,14 +5,34 @@ class Body extends Component {
     render() {
       return (
         <Div>
-
+          <Title>About Me</Title>
+          <Subtitle>WHO AM I?</Subtitle>
         </Div>
       )
     }
 }
 
 const Div = styled.div`
+  background-color: green;
+  font-family: "Times";
+  height: 85px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
 
+const Title = styled.p`
+  text-align: center;
+  background-color: transparent;
+  font-family: "Times";
+  font-weight: bold;
+  font-size: 25px;
+  text-align: center;
+`;
+
+const Subtitle = styled.p`
+  
 `;
 
 export default Body;
