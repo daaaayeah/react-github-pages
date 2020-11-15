3ef4bf07a08953412edf18df3b67632b0bb579cb
daye <leeda1801@gmail.com>
2020-11-15 23:58:17 +0900

    Add about area

--- src/Body.js
+++ src/Body.js
@@ -5,8 +5,10 @@ class Body extends Component {
     render() {
       return (
         <Div>
-          <Title>About Me</Title>
-          <Subtitle>WHO AM I?</Subtitle>
+          <About>
+            <Title>ABOUT ME</Title>
+            <Subtitle>Who am I?</Subtitle>
+          </About>
         </Div>
       )
     }
@@ -14,25 +16,34 @@ class Body extends Component {
 
 const Div = styled.div`
   background-color: green;
-  font-family: "Times";
-  height: 85px;
+  height: 382px;
   position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
+  top: 85;
+  width: 100%;
+`;
+
+const About = styled.div`
+  background-color: white;
+  position: fixed;
+  top: 85;
+  width: 100%;
+  padding-left: 12.5%;
+  padding-right: 12.5%;
 `;
 
 const Title = styled.p`
-  text-align: center;
   background-color: transparent;
-  font-family: "Times";
-  font-weight: bold;
   font-size: 25px;
+  font-family: Roboto;
+  font-weight: bold;
   text-align: center;
 `;
 
 const Subtitle = styled.p`
-  
+  background-color: transparent;
+  font-size: 20px;
+  font-family: Roboto;
+  font-weight: bold;
 `;
 
 export default Body;
