28666ed1520d90eaa642069a1039d130d5dd38df
daye <leeda1801@gmail.com>
2020-11-16 22:50:58 +0900

    (Try) scroll view

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <div>
+    <div style={{ height: '100%', overflowY: 'scroll' }}>
       <Header />
       <Body />
       <Footer />
