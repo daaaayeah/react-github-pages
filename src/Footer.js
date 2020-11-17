8e6cc3a963463eea6a6106ff48a6eb5bf4d3ae26
daye <leeda1801@gmail.com>
2020-11-17 17:18:20 +0900

    Modify copyright position

--- src/Footer.js
+++ src/Footer.js
@@ -5,32 +5,34 @@ class Footer extends Component {
 	render() {
 		return (
 			<Div>
-				<div>
-					<Contact>CONTACT</Contact>
-					<P>If you have any questions,</P>
-					<P>please leave your message here.</P>
-					<CopyRight>
-						COPYRIGHT INFORMATION GOES HERE © 2020. ALL RIGHTS RESERVED
-					</CopyRight>
-				</div>
-				<div style={{ display: "flex" }}>
-					<div style={{ margin: "40px 0px", width: "250px" }}>
-						<Input type="text" placeholder="NAME" style={{ width: "190px" }} />
-						<Input
-							type="text"
-							placeholder="E-MAIL"
-							style={{ width: "190px" }}
-						/>
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+					<div>
+						<Contact>CONTACT</Contact>
+						<P>If you have any questions,</P>
+						<P>please leave your message here.</P>
 					</div>
-					<div style={{ margin: "40px 0px" }}>
-						<Textarea
-							type="text"
-							placeholder="MESSAGE"
-							style={{ width: "420px", height: "75px" }}
-						/>
+					<div style={{ display: "flex" }}>
+						<div style={{ margin: "40px 0px", width: "250px" }}>
+							<Input type="text" placeholder="NAME" style={{ width: "190px" }} />
+							<Input
+								type="text"
+								placeholder="E-MAIL"
+								style={{ width: "190px" }}
+							/>
+						</div>
+						<div style={{ margin: "40px 0px" }}>
+							<Textarea
+								type="text"
+								placeholder="MESSAGE"
+								style={{ width: "420px", height: "75px" }}
+							/>
+						</div>
+						<Button type="submit">SUBMIT</Button>
 					</div>
-					<Button type="submit">SUBMIT</Button>
 				</div>
+				<CopyRight>
+					COPYRIGHT INFORMATION GOES HERE © 2020. ALL RIGHTS RESERVED
+				</CopyRight>
 			</Div>
 		);
 	}
@@ -39,8 +41,6 @@ class Footer extends Component {
 const Div = styled.div`
 	background-color: green;
 	height: 255px;
-	display: flex;
-	justify-content: space-between;
 	padding-left: 12.5%;
 	padding-right: 12.5%;
 `;
@@ -107,10 +107,7 @@ const CopyRight = styled.p`
 	color: #c8dbc8;
 	background-color: transparent;
 	font-size: 13px;
-
-	position: fixed;
-	bottom: 0;
-	padding: 12px 0px 12px 0px;
+	margin: 0px;
 `;
 
 export default Footer;
