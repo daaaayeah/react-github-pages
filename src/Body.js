ee719acf0d68e4484dfe87144c8aced3e11592ea
daye <leeda1801@gmail.com>
2020-11-18 19:05:13 +0900

    Add ability and portfolio

--- src/Body.js
+++ src/Body.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import About from "./About";
+import Ability from "./Ability";
+import Portfolio from "./Portfolio";
 
 class Body extends Component {
   render() {
     return (
       <Div>
         <About />
+        <Ability />
+        <Portfolio />
       </Div>
     );
   }
 }
 
 const Div = styled.div`
-	background-color: green;
+  background-color: green;
 	width: 100%;
 `;
 
