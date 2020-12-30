cb493eebb152176c8b82f2440ce5eb0c3e53a3a4
daye <leeda1801@gmail.com>
2020-12-30 23:46:23 +0900

    Add content

--- src/Ability.js
+++ src/Ability.js
@@ -1,83 +1,123 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 function Ability() {
     useEffect(() => {
         AOS.init();
         AOS.refresh();
-      }, []);
+    }, []);
 
     return (
         <Div id="ability">
             <Title>________</Title>
             <Title>PROGRAMMING ABILITY</Title>
-            <div style={{ margin: "75px 0px 75px 0px", display: "flex", justifyContent: "space-between" }}>
-                <Box data-aos="flip-left" style={{ backgroundColor: "orange", borderRadius: "50%" }}>
+            <div
+                style={{
+                    margin: "75px 0px 75px 0px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Box
+                    data-aos="flip-left"
+                    style={{ backgroundColor: "orange", borderRadius: "50%" }}
+                >
                     <Item>C</Item>
                     <Item>C#</Item>
                 </Box>
-                <Box data-aos="flip-down" style={{ backgroundColor: "#ffdead", borderRadius: "50%" }}>
+                <Box
+                    data-aos="flip-down"
+                    style={{ backgroundColor: "#ffdead", borderRadius: "50%" }}
+                >
                     <Item>HTML</Item>
                     <Item>CSS</Item>
                     <Item>JavaScript</Item>
+                    <Item>TypeScript</Item>
                     <Item>React</Item>
                 </Box>
-                <Box data-aos="flip-right" style={{ backgroundColor: "#ffdead", borderRadius: "50%" }}>
+                <Box
+                    data-aos="flip-right"
+                    style={{ backgroundColor: "#ffdead", borderRadius: "50%" }}
+                >
                     <Item>Java</Item>
                 </Box>
-                <Box data-aos="flip-up" style={{ backgroundColor: "oldlace", borderRadius: "50%" }}>
+                <Box
+                    data-aos="flip-up"
+                    style={{ backgroundColor: "oldlace", borderRadius: "50%" }}
+                >
                     <Item>OpenGL</Item>
                     <Item>WebGL</Item>
                 </Box>
-                <Box data-aos="flip-left" style={{ backgroundColor: "orange", borderRadius: "50%" }}>
+                <Box
+                    data-aos="flip-left"
+                    style={{ backgroundColor: "orange", borderRadius: "50%" }}
+                >
                     <Item>Python</Item>
                 </Box>
-                <Box data-aos="flip-down" style={{ backgroundColor: "white", borderRadius: "50%" }}>
+                <Box
+                    data-aos="flip-down"
+                    style={{ backgroundColor: "white", borderRadius: "50%" }}
+                >
                     <Item>SQL</Item>
                 </Box>
             </div>
-            <div style={{ margin: "50px 0px 50px 0px", display: "flex", justifyContent: "space-between" }}>
-                <Box data-aos="flip-down" style={{ backgroundColor: "oldlace" }} >
+            <div
+                style={{
+                    margin: "50px 0px 50px 0px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Box
+                    data-aos="flip-down"
+                    style={{ backgroundColor: "oldlace" }}
+                >
                     <Item>Android Studio</Item>
                 </Box>
-                <Box data-aos="flip-left" style={{ backgroundColor: "#ffdead" }} >
+                <Box
+                    data-aos="flip-left"
+                    style={{ backgroundColor: "#ffdead" }}
+                >
                     <Item>Eclipse</Item>
                 </Box>
-                <Box data-aos="flip-up" style={{ backgroundColor: "orange" }} >
+                <Box data-aos="flip-up" style={{ backgroundColor: "orange" }}>
                     <Item>Jupyter notebook</Item>
                 </Box>
-                <Box data-aos="flip-right" style={{ backgroundColor: "oldlace" }} >
+                <Box
+                    data-aos="flip-right"
+                    style={{ backgroundColor: "oldlace" }}
+                >
                     <Item>MySQL workbench</Item>
                 </Box>
-                <Box data-aos="flip-down" style={{ backgroundColor: "white" }} >
+                <Box data-aos="flip-down" style={{ backgroundColor: "white" }}>
                     <Item>Unity</Item>
                 </Box>
-                <Box data-aos="flip-left" style={{ backgroundColor: "orange" }} >
+                <Box data-aos="flip-left" style={{ backgroundColor: "orange" }}>
                     <Item>Visual Studio</Item>
                 </Box>
             </div>
-        </Div >
-    )
+        </Div>
+    );
 }
 
 const Div = styled.div`
-	background-color: white;
+    background-color: white;
     padding: 1px 12.5% 1px 12.5%;
 `;
 
 const Title = styled.p`
-	color: green;
-	font-size: 20px;
-	font-family: Trebuchet;
-	font-weight: bold;
+    color: green;
+    font-size: 20px;
+    font-family: Trebuchet;
+    font-weight: bold;
 `;
 
 const Box = styled.div`
     font-size: 17px;
     text-align: center;
-    
+
     height: 150px;
     width: 150px;
     display: flex;
--- src/About.js
+++ src/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 import programmingImage from "./images/programming.jpg";
 import myImage from "./images/I.jpg";
@@ -9,7 +9,7 @@ function About() {
     useEffect(() => {
         AOS.init();
         AOS.refresh();
-      }, []);
+    }, []);
 
     return (
         <Div id="about">
@@ -17,45 +17,116 @@ function About() {
             <Title>ABOUT DAYE</Title>
             <Content style={{ justifyContent: "space-between" }}>
                 <div data-aos="fade-right" data-aos-duration="1500">
-                    <Subtitle style={{ margin: "130px 0px 30px 0px" }}>&lt;Who am I?&nbsp;/&gt;</Subtitle>
+                    <Subtitle style={{ margin: "130px 0px 30px 0px" }}>
+                        &lt;Who am I?&nbsp;/&gt;
+                    </Subtitle>
                     <P>반갑습니다.</P>
-                    <P><b>세종대학교</b>에서 <b>컴퓨터공학</b>을 전공하고 있는 <b>이다예</b>입니다.</P>
+                    <P>
+                        <b>세종대학교</b>에서 <b>컴퓨터공학</b>을 전공하고 있는{" "}
+                        <b>이다예</b>입니다.
+                    </P>
                     <P>무엇이든 새로운 것에 흥미를 느낍니다.</P>
-                    <P>끊임없이 변화하는 프로그래밍의 세상 속에서 오랫동안 공부하며</P>
-                    <P><b>세계를 움직이는 사람</b>이 되고 싶습니다.</P>
+                    <P>
+                        끊임없이 변화하는 프로그래밍의 세상 속에서 오랫동안
+                        공부하며
+                    </P>
+                    <P>
+                        <b>세계를 움직이는 사람</b>이 되고 싶습니다.
+                    </P>
                 </div>
-                <img data-aos="fade-left" data-aos-duration="1500" src={programmingImage} width="600px" height="300px" alt="" />
+                <img
+                    data-aos="fade-left"
+                    data-aos-duration="1500"
+                    src={programmingImage}
+                    width="600px"
+                    height="300px"
+                    alt=""
+                />
             </Content>
             <Content>
-                <img data-aos="fade-right" data-aos-duration="2000" src={myImage} width="600px" height="300px" alt="" />
-                <div data-aos="fade-left" data-aos-duration="2000" style={{ marginLeft: "50px" }}>
-                    <Subtitle style={{ margin: "30px 0px 40px 0px" }}>&lt;My History&nbsp;/&gt;</Subtitle>
-                    <P style={{ margin: "20px 0px" }}>
-                        <big><big><big><b style={{ color: "green", fontFamily: "serif" }}>2020</b></big></big></big>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            한국대학생IT경영학회 22기 회원
-                        </P>
+                <img
+                    data-aos="fade-right"
+                    data-aos-duration="2000"
+                    src={myImage}
+                    width="600px"
+                    height="300px"
+                    alt=""
+                />
+                <div
+                    data-aos="fade-left"
+                    data-aos-duration="2000"
+                    style={{ marginLeft: "50px" }}
+                >
+                    <Subtitle style={{ margin: "25px 0px 40px 0px" }}>
+                        &lt;My History&nbsp;/&gt;
+                    </Subtitle>
                     <P style={{ margin: "20px 0px 10px 0px" }}>
-                        <big><big><big><b style={{ color: "green", fontFamily: "serif" }}>2019</b></big></big></big>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            컴퓨터공학과 제 31대 학생회 '.com' 부학생회장
-                        </P>
+                        <big>
+                            <big>
+                                <big>
+                                    <b
+                                        style={{
+                                            color: "green",
+                                            fontFamily: "serif",
+                                        }}
+                                    >
+                                        2020
+                                    </b>
+                                </big>
+                            </big>
+                        </big>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; (주)보바 프론트엔드
+                        개발자
+                    </P>
+                    <P style={{ marginLeft: "85px" }}>
+                        &nbsp;한국대학생IT경영학회 22기 회원
+                    </P>
+                    <P style={{ margin: "20px 0px 10px 0px" }}>
+                        <big>
+                            <big>
+                                <big>
+                                    <b
+                                        style={{
+                                            color: "green",
+                                            fontFamily: "serif",
+                                        }}
+                                    >
+                                        2019
+                                    </b>
+                                </big>
+                            </big>
+                        </big>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 컴퓨터공학과 제 31대
+                        학생회 '.com' 부학생회장
+                    </P>
                     <P style={{ marginLeft: "85px" }}>
                         &nbsp;컴퓨터공학과 제 31대 학생회 '.com' 집부
-                        </P>
+                    </P>
                     <P style={{ margin: "20px 0px" }}>
-                        <big><big><big><b style={{ color: "green", fontFamily: "serif" }}>2018</b></big></big></big>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            세종대학교 입학
-                        </P>
+                        <big>
+                            <big>
+                                <big>
+                                    <b
+                                        style={{
+                                            color: "green",
+                                            fontFamily: "serif",
+                                        }}
+                                    >
+                                        2018
+                                    </b>
+                                </big>
+                            </big>
+                        </big>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 세종대학교 입학
+                    </P>
                 </div>
             </Content>
         </Div>
-    )
+    );
 }
 
 const Div = styled.div`
-	background-color: white;
+    background-color: white;
     padding: 1px 12.5% 1px 12.5%;
 `;
 
@@ -65,23 +136,23 @@ const Content = styled.div`
 `;
 
 const Title = styled.p`
-	color: green;
-	font-size: 20px;
-	font-family: Trebuchet;
+    color: green;
+    font-size: 20px;
+    font-family: Trebuchet;
     font-weight: bold;
 `;
 
 const Subtitle = styled.p`
-	color: orange;
-	font-size: 17px;
+    color: orange;
+    font-size: 17px;
     font-family: Verdana;
 `;
 
 const P = styled.p`
-	color: black;
+    color: black;
     font-size: 15px;
     font-family: paybooc-Medium;
-	margin: 5px;
+    margin: 5px;
 `;
 
 export default About;
